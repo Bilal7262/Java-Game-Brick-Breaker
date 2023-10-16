b47b851cf75e01f3cbff017ee790e9b49b7ac275
Bilal Bashir <bilalbashir95@gmail.com>
2023-10-16 17:22:33 +0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import java.awt.Color;
-
 import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String[] args) {
-		JFrame obj=new JFrame();
-		Gameplay gamePlay = new Gameplay();
-		
-		obj.setBounds(10, 10, 700, 600);
-		obj.setTitle("Breakout Ball");		
-		obj.setResizable(false);
-		obj.setVisible(true);
-		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		obj.add(gamePlay);
-                obj.setVisible(true);
-		
-	}
+    public static void main(String[] args) {
+        // Create a JFrame window
+        JFrame obj = new JFrame();
+        
+        // Create an instance of the Gameplay class
+        Gameplay gamePlay = new Gameplay();
 
+        // Set JFrame properties
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add the Gameplay object to the JFrame
+        obj.add(gamePlay);
+        
+        // Make the JFrame visible
+        obj.setVisible(true);
+    }
 }
--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -2,49 +2,53 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class MapGenerator 
-{
-	public int map[][];
-	public int brickWidth;
-	public int brickHeight;
-	
-	public MapGenerator (int row, int col)
-	{
-		map = new int[row][col];		
-		for(int i = 0; i<map.length; i++)
-		{
-			for(int j =0; j<map[0].length; j++)
-			{
-				map[i][j] = 1;
-			}			
-		}
-		
-		brickWidth = 540/col;
-		brickHeight = 150/row;
-	}	
-	
-	public void draw(Graphics2D g)
-	{
-		for(int i = 0; i<map.length; i++)
-		{
-			for(int j =0; j<map[0].length; j++)
-			{
-				if(map[i][j] > 0)
-				{
-					g.setColor(Color.white);
-					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
-					
-					// this is just to show separate brick, game can still run without it
-					g.setStroke(new BasicStroke(3));
-					g.setColor(Color.black);
-					g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);				
-				}
-			}
-		}
+public class MapGenerator {
+    public int[][] map;
+    public int brickWidth;
+    public int brickHeight;
+
+    public MapGenerator(int row, int col) {
+        map = new int[row][col];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = 1;
+            }
+        }
+
+        brickWidth = 540 / col;
+        brickHeight = 150 / row;
+    }
+
+    public void draw(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(Color.white);
+                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+
+                    // To show separate bricks (optional)
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
+    }
+
+	// Add getter methods to provide access to these fields
+	public int[][] getMap() {
+		return map;
 	}
-	
-	public void setBrickValue(int value, int row, int col)
-	{
-		map[row][col] = value;
+
+	public int getBrickWidth() {
+		return brickWidth;
+	}
+
+	public int getBrickHeight() {
+		return brickHeight;
 	}
 }
